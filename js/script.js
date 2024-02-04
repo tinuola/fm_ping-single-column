d0241ce27f6c1329960d002f67e599b1b0493a47
tinuola <24995224+tinuola@users.noreply.github.com>
2024-02-03 23:31:06 -0800

    feat: implement form validation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,73 @@
-console.log('Hello!')
+let emailInput = document.getElementById( 'email-input' )
+let emailInputData = null
+
+let errMsgDisplay = document.querySelectorAll( '.error-msg' )
+let errMsgMobile = document.getElementById( 'error-msg__mobile' )
+let errMsgDesktop = document.getElementById( 'error-msg__desktop' )
+
+let submitBtn = document.querySelector( 'button' )
+
+let emailValidityStatus = null
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+submitBtn.addEventListener( 'click', validateEmail )
+
+
+function displayErrorMsg () {
+  let msg = `Please provide a valid email address`
+
+  if ( window.innerWidth < 800 ) {
+
+    errMsgDisplay[ 0 ].style.display = 'block'
+    errMsgMobile.innerHTML = msg
+
+    errMsgDisplay[ 1 ].style.display = 'none'
+    errMsgDesktop.innerHTML = ''
+
+  } else {
+
+    errMsgDisplay[ 1 ].style.display = 'block'
+    errMsgDesktop.innerHTML = msg
+
+    errMsgDisplay[ 0 ].style.display = 'none'
+    errMsgMobile.innerHTML = ''
+  }
+}
+
+
+function clearErrorState () {
+
+  if ( !validEmailStatus || emailInput.length < 1 ) {
+    emailInput.classList.remove( 'error-state' )
+    errMsgDisplay.forEach( field => field.style.display = 'none' )
+    window.removeEventListener( 'resize', displayErrorMsg )
+  }
+
+}
+
+
+function validateEmail ( e ) {
+  e.preventDefault();
+
+  emailInputData = emailInput.value
+
+  if ( emailInputData.length === 0 || !emailRegex.test( emailInputData ) ) {
+
+    validEmailStatus = false
+
+    emailInput.classList.add( 'error-state' )
+
+    displayErrorMsg()
+
+    window.addEventListener( 'resize', displayErrorMsg )
+
+    emailInput.addEventListener( 'keyup', clearErrorState )
+
+  } else {
+
+    emailInput.value = ''
+
+  }
+
+}
